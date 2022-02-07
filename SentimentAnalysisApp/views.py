20920c0ca0e972c63a3fc7f142fb71b1342f5289
Siddhant Mahalle <siddhantmahalle4@gmail.com>
2022-02-07 18:26:31 +0530

    Created views for Home page and Result Page

--- SentimentAnalysisApp/views.py
+++ SentimentAnalysisApp/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from .ML_Files.Prediction import predict
+
 
 # Create your views here.
+
+def home(request):
+    return render(request, 'index.html')
+
+
+def result(request):
+    text = (str(request.GET['text']))
+    # print(text)
+    prediction = predict(text)
+    print(prediction)
+    result = ''
+    if prediction == 1:
+        result = 'Positive'
+    else:
+        result = 'Negative'
+
+    return render(request, 'result.html', {'result': result})
